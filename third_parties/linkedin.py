ca24b9f193fc598589f22116f8b1c515784f4806
Phillipp Bertram <me@phillippbertram.de>
2025-05-15 12:52:23 +0200

    ice_breaker implementation

---
+++ third_parties/linkedin.py
@@ -0,0 +1,37 @@
+import os
+import json
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+    """scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinkedIn profile"""
+
+    if mock:
+        # read from /mock/linkedin_phillippbertram.json
+        with open("mock/linkedin_phillippbertram.json", "r") as f:
+            return json.load(f)
+    
+    api_key = os.environ["PROXYCURL_API_KEY"]
+    headers = {"Authorization": "Bearer " + api_key}
+    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    params = {
+        'linkedin_profile_url': linkedin_profile_url,
+    }
+    response = requests.get(api_endpoint,
+                            params=params,
+                            headers=headers)
+
+    return response.json()
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/phillipp-bertram/",
+            mock=True,
+        ),
+    )
